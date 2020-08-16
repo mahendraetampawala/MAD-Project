28c807f93237055191905c1d53005913a27ed282
Hasantha32 <hasanthamadushan32@gmail.com>
2020-08-17 04:35:40 +0530

    InitialCommit

--- app/src/main/java/com/example/callrecorder/MainActivity.java
+++ app/src/main/java/com/example/callrecorder/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
@@ -13,6 +15,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView3=findViewById(R.id.textView3);
+        textView3.setText("Call Recorder");
+
+        TextView btn1=findViewById(R.id.button1);
+        btn1.setText("1");
+
+        TextView btn2=findViewById(R.id.button2);
+        btn2.setText("2");
+
+        TextView btn3=findViewById(R.id.button3);
+        btn3.setText("3");
+
+        TextView btn4=findViewById(R.id.button4);
+        btn4.setText("4");
+
+        TextView btn5=findViewById(R.id.button5);
+        btn5.setText("5");
+
+        TextView btn6=findViewById(R.id.button6);
+        btn6.setText("6");
+
+        TextView btn7=findViewById(R.id.button7);
+        btn7.setText("7");
+
+        TextView btn8=findViewById(R.id.button8);
+        btn8.setText("8");
+
+        TextView btn9=findViewById(R.id.button40);
+        btn9.setText("9");
+
+        TextView btn0=findViewById(R.id.button0);
+        btn0.setText("0");
 
         button = (Button) findViewById(R.id.button36);
         button.setOnClickListener(new View.OnClickListener() {
